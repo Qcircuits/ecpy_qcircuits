14a384972332cb9bd2eb051a5ca7d75f8adfd17e
Qcircuits <phymeso@gmail.com>
2018-12-03 09:35:17 +0100

    Added functionality for Tabor AWG and Rohde Schwarz source with IQmixer.

---
+++ ecpy_qcircuits/tasks/tasks/instr/AWG_tasks.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+# -----------------------------------------------------------------------------
+# Copyright 2015-2016 by EcpyHqcLegacy Authors, see AUTHORS for more details.
+#
+# Distributed under the terms of the BSD license.
+#
+# The full license is in the file LICENCE, distributed with this software.
+# -----------------------------------------------------------------------------
+"""Sets AWG parameters.
+
+"""
+from __future__ import (division, unicode_literals, print_function,
+                        absolute_import)
+
+#import numpy as np
+#from atom.api import set_default
+
+from ecpy.tasks.api import InstrumentTask
+from atom.api import Unicode
+
+class AWGSetDCOffsetTask(InstrumentTask):
+    """ Set the DC offset voltage of a given AWG channel
+
+    """    
+    
+    channel = Unicode('1').tag(pref=True)
+    voltage = Unicode('0').tag(pref=True)
+
+    def perform(self):
+        """
+        """
+        if not self.driver:
+            self.start_driver()
+        
+        channel_driver = self.driver.get_channel(int(self.channel))
+        channel_driver.set_DC_offset( \
+                        float(self.format_and_eval_string(self.voltage)))
+        
+class AWGSetVppTask(InstrumentTask):
+    """ Set the Vpp of a given AWG channel
+
+    """    
+    
+    channel = Unicode('1').tag(pref=True)
+    amplitude = Unicode('0').tag(pref=True)
+
+    def perform(self):
+        """
+        """
+        if not self.driver:
+            self.start_driver()
+        
+        channel_driver = self.driver.get_channel(int(self.channel))
+        channel_driver.set_Vpp(float(self.format_and_eval_string(self.
+                                                                 amplitude)))
